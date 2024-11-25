b03d1af4b89bde307008938bc9eb8f2d00ac309f
Vadym16 <117903952+Vadym16@users.noreply.github.com>
2024-11-25 01:23:39 +0100

    Add project files.

---
+++ LBB №1.1/LBB №1.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "ukr");
+
+    int N, sum=0;
+    float averg;
+    cout << "Введіть розмір масиву: "; cin >> N;
+    int A[100];
+    cout << "Масив A: ";
+    for (int i = 0; i < N; i++) {
+        A[i] = rand() % 20 - 10;
+        cout << A[i] << " ";
+        sum += A[i];
+    }
+
+    averg = sum / N;
+    cout << "\nСереднє Арефметичне елементів масиву A: " << averg;
+
+
+
+}
+
+
